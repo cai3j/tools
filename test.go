8d6ce70bbbf03b04b64b18d937fc30999d1bbe51
王蕾 <wanglei@wangleideAir.lan>
2017-05-19 23:47:21 +0800

    something

--- test.go
+++ test.go
@@ -11,6 +11,8 @@ import (
 	"log"
 	"os"
 	"bufio"
+	"sync"
+	"encoding/binary"
     "github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
@@ -53,23 +55,89 @@ func scan(iface *net.Interface) error {
 	
 	return nil
 }
-func cliOrder(order chan string) error{
-	running := true
+
+func cliOrder(order_chan chan string) {
+    running := true
     reader := bufio.NewReader(os.Stdin)
     for running {
         data, _, _ := reader.ReadLine()
         command := string(data)
         if command == "stop" {
             running = false
+            continue
         }
-        if command == "stoppacket"{
-        	order <- "stoppacket"
+        if len(command) > 0 {
+        	order_chan <- command
         }
-        log.Println("command", command)
+        log.Println("command:", command)
     }
-    return nil
+    os.Exit(0)
 }
-func packetFun(args []string) error {
+
+func readPacket(handle *pcap.Handle, stop chan string) {
+	src := gopacket.NewPacketSource(handle, layers.LayerTypeEthernet) //读取报文
+	in := src.Packets()
+	for {
+		var packet gopacket.Packet
+		var order string
+		select {
+		case order = <-stop: // 如果是读到停止， 就返回
+			log.Print("Read a command : ",order)
+			if order == "quit" {
+				return
+			}
+		case packet = <-in:
+			icmpLayer := packet.Layer(layers.LayerTypeICMPv4)
+			if icmpLayer == nil {
+				continue
+			}
+			log.Printf("pkt : %+v", packet)
+			//icmp := icmpLayer.(*layers.ICMPv4)
+			//log.Printf("%+v",icmp)
+			//log.Printf("IP %v is at %v", net.IP(arp.SourceProtAddress), net.HardwareAddr(arp.SourceHwAddress))
+		}
+	}
+}
+
+func ExePcap(args []string) {
+	log.Printf("ExePcap args : %v", args)
+	
+
+	intfs, err := net.Interfaces()
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Printf("inter %+v\n", intfs)
+	for _, intf := range intfs {
+		fmt.Printf("inter %v : %+v\n", intf.Name, intf)
+	}
+
+	fmt.Println("Select a interface : ", *intf)
+
+	handle, err := pcap.OpenLive(*intf, 65536, true, pcap.BlockForever)
+	if err != nil {
+		panic(err)
+	}
+	defer handle.Close()
+	var wg sync.WaitGroup
+	
+	order_chan := make(chan string)
+	go func() {
+		cliOrder(order_chan)
+	}()
+	defer close(order_chan)
+	
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		readPacket(handle, order_chan)
+		log.Print("Read packet done!")
+	}()
+	log.Print("Init OK!")
+	wg.Wait()
+}
+
+func ExePacket(args []string) error {
 	fmt.Printf("test order : %v\n", args)
 	order_chan := make(chan string) 
 	go cliOrder(order_chan)
@@ -126,7 +194,24 @@ func packetFun(args []string) error {
 		}
 	}
 }
-func testFun(args []string) error {
+
+var retmap map[string]interface{}
+
+func test(order string, args ...string)  map[string]interface{} {
+	fmt.Printf("string : %s\n", order)
+	for i,v := range args {
+		fmt.Printf("%d : %s\n", i,v)
+	}
+	if retmap == nil{
+		retmap = make(map[string]interface{})
+	}
+	retmap["123"] = 1
+	retmap["234"] = "DE"
+	log.Printf(" test ret : %v", retmap)
+	return retmap
+}
+
+func ExeTest(args []string) error {
 	fmt.Printf("test order : %v\n", args)
 	fmt.Println("Int :",*intFlag)
 	fmt.Println("String : ", *strFlag)
@@ -134,13 +219,26 @@ func testFun(args []string) error {
 		return nil
 	}
 	fmt.Println("Hello world")
-	
+	if ok,err := regexp.MatchString(`\d`, "a1bc"); err == nil && ok  {
+		log.Printf("match \n")
+	}
+	ilist := []string{"order"}
+	retv := test(ilist[0],ilist[1:]...)
+	log.Printf(" test : %v", retv)
+	a := 1
+	if a > 0 {
+		fmt.Println("A > 0")
+	} else if a < 0 {
+		fmt.Println("A < 0")
+	} else {
+		fmt.Println("A = 0")
+	}
 	var t1 = time.Now()
 	var t2 = t1.Add(time.Minute)
 	fmt.Printf("Time : %v , %v\n",t1.Format(time.RFC3339),t2)
 	return nil
 }
-func intfFun(args []string) error {
+func ExeNet(args []string) error {
 	fmt.Printf("intf order : %v\n", args)
 	ifaces, err := net.Interfaces()
 	if err != nil {
@@ -156,7 +254,7 @@ func intfFun(args []string) error {
 	return nil
 }
 
-func regexpFun(args []string) error{
+func ExeRegexp(args []string) error{
 	fmt.Printf("regexpFun : %v\n", args)
 	ret,_ := regexp.MatchString("a.b","acb")
 	fmt.Printf("regexp : %+v\n",ret)
@@ -175,7 +273,7 @@ func regexpFun(args []string) error{
 	return nil
 }
 
-func strFun(args []string) error{
+func ExeStr(args []string) error{
 	log.Printf("strFun : %v\n", args)
 	var str1 string = "123"
 	var str2 string = "123"
@@ -186,95 +284,9 @@ func strFun(args []string) error{
 	return nil
 }
 
-func cliFun(args []string) error{
-	log.Printf("cliFun : %v\n", args)
-	running := true
-    reader := bufio.NewReader(os.Stdin)
-    for running {
-        data, _, _ := reader.ReadLine()
-        command := string(data)
-        if command == "stop" {
-            running = false
-        }
-        log.Println("command", command)
-    }
-    return nil
-}
-func readPacket(handle *pcap.Handle, stop chan string) {
-	src := gopacket.NewPacketSource(handle, layers.LayerTypeEthernet) //读取报文
-	in := src.Packets()
-	for {
-		var packet gopacket.Packet
-		var order string
-		select {
-		case order = <-stop: // 如果是读到停止， 就返回
-			log.Print("Read a command : ",order)
-			if order == "quit" {
-				return
-			}
-		case packet = <-in:
-			icmpLayer := packet.Layer(layers.LayerTypeICMPv4)
-			if icmpLayer == nil {
-				continue
-			}
-			log.Printf("pkt : %+v", packet)
-			//icmp := icmpLayer.(*layers.ICMPv4)
-			//log.Printf("%+v",icmp)
-			//log.Printf("IP %v is at %v", net.IP(arp.SourceProtAddress), net.HardwareAddr(arp.SourceHwAddress))
-		}
-	}
-}
-
-func cliOrder(order_chan chan string) {
-    running := true
-    reader := bufio.NewReader(os.Stdin)
-    for running {
-        data, _, _ := reader.ReadLine()
-        command := string(data)
-        if command == "stop" {
-            running = false
-            continue
-        }
-        if len(command) > 0 {
-        	order_chan <- command
-        }
-        log.Println("command:", command)
-    }
-    os.Exit(0)
-}
-
-var retmap map[string]interface{}
-
-func test(order string, args ...string)  map[string]interface{} {
-	fmt.Printf("string : %s\n", order)
-	for i,v := range args {
-		fmt.Printf("%d : %s\n", i,v)
-	}
-	if retmap == nil{
-		retmap = make(map[string]interface{})
-	}
-	retmap["123"] = 1
-	retmap["234"] = "DE"
-	log.Printf(" test ret : %v", retmap)
-	return retmap
-}
-
-func ExeTest(args []string) {
+func ExeCli(args []string) {
 	log.Printf("ExeTest args : %v", args)
-	if ok,err := regexp.MatchString('\d', "a1bc"); err == nil && ok  {
-		log.Printf("match \n")
-	}
-	ilist := []string{"order"}
-	retv := test(ilist[0],ilist[1:]...)
-	log.Printf(" test : %v", retv)
-	a := 1
-	if a > 0 {
-		fmt.Println("A > 0")
-	} else if a < 0 {
-		fmt.Println("A < 0")
-	} else {
-		fmt.Println("A = 0")
-	}
+	
 	running := true
 	chs := make(chan int)
 	reader := bufio.NewReader(os.Stdin)
@@ -297,44 +309,6 @@ func ExeTest(args []string) {
 		log.Println("command : ", command)
 	}
 	defer close(chs)
-}
-
-func ExePcap(args []string) {
-	log.Printf("ExePcap args : %v", args)
-	
-
-	intfs, err := net.Interfaces()
-	if err != nil {
-		panic(err)
-	}
-	//fmt.Printf("inter %+v\n", intfs)
-	for _, intf := range intfs {
-		fmt.Printf("inter %v : %+v\n", intf.Name, intf)
-	}
-
-	fmt.Println("Select a interface : ", *intf)
-
-	handle, err := pcap.OpenLive(*intf, 65536, true, pcap.BlockForever)
-	if err != nil {
-		panic(err)
-	}
-	defer handle.Close()
-	var wg sync.WaitGroup
-	
-	order_chan := make(chan string)
-	go func() {
-		cliOrder(order_chan)
-	}()
-	defer close(order_chan)
-	
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		readPacket(handle, order_chan)
-		log.Print("Read packet done!")
-	}()
-	log.Print("Init OK!")
-	wg.Wait()
 }
 
 func ExeUdps(args []string){
@@ -421,25 +395,23 @@ func main() {
 	
 	switch order {
 		case "test" :
-			testFun(flag.Args()[1:])
-		case "intf" :
-			intfFun(flag.Args()[1:])
+			ExeTest(flag.Args()[1:])
+		case "net" :
+			ExeNet(flag.Args()[1:])
 		case "regexp" :
-			regexpFun(flag.Args()[1:])
-		case "cli":
-			cliFun(flag.Args()[1:])
+			ExeRegexp(flag.Args()[1:])
 		case "str" :
-			strFun(flag.Args()[1:])	
+			ExeStr(flag.Args()[1:])	
 		case "packet" :
-			packetFun(flag.Args()[1:])	
+			ExePacket(flag.Args()[1:])	
 		case "udps":
 			ExeUdps(args)
 		case "udpc":
 			ExeUdpc(args)
 		case "pcap":
 			ExePcap(args)
-		case "test2":
-			ExeTest(args)
+		case "cli2":
+			ExeCli(args)
 		default :
 			flag.PrintDefaults()
 	}
