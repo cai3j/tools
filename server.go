aae6e07f9f2b29996c971e22cff8372067cd1c5a
王蕾 <wanglei@wangleideAir.lan>
2018-01-29 20:27:32 +0800

    server.go

---
+++ server.go
@@ -0,0 +1,19 @@
+
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w,
+     "Hi, This is an example of http service in golang!")
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    //http.ListenAndServe(":8080", nil)
+    http.ListenAndServeTLS(":8081", "server.crt",
+                           "server.key", nil)
+}
